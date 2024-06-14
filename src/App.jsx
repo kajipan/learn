b56c267130e32af861cf2179b2837559b682802f
kajipan <rskajipan1101@gmail.com>
2024-06-14 12:56:56 +0530

    3rd

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,6 @@ function App() {
         </a>
       </div>
       <h1>kaji</h1>
-      <h1>kaji</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
